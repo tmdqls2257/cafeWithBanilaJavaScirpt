6d4067864d2f04d6d271850ac103747d50b9b658
Seung Bin Hong <tmdqls2257@gmail.com>
2022-05-13 18:40:46 +0900

    수정 및 삭제 기능 구현

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ const $ = selecter => document.querySelector(selecter)
 function App() {
   // form 태그가 자동으로 전송되는 것을 막아준다.
   const menuForm = $('#espresso-menu-form')
-  menuForm.addEventListener('submit', event => {
-    event.preventDefault()
-  })
-  //   메뉴를 입력 받는다.
-  const coffeMenu = $('#espresso-menu-name')
-  coffeMenu.addEventListener('keypress', event => {
-    if (event.key === 'Enter') {
-      insertList()
-    }
-    const menuButton = $('#espresso-menu-submit-button')
-    menuButton.addEventListener('click', () => {
-      insertList()
-    })
+  menuForm.addEventListener('submit', e => {
+    e.preventDefault()
   })
 
-  function insertList() {
+  function insertCoffeeMenu() {
     const expressoMenuName = coffeMenu.value
     const menuItemTemplate = expressoMenuName => {
       return `<li class="menu-list-item d-flex items-center py-2">
@@ -37,10 +26,54 @@ function App() {
     </button>
   </li>`
     }
-    coffeMenu.value = ''
     // $('#espresso-menu-list').insertAdjacentHTML('beforebegin', menuItemTemplate(expressoMenuName))
 
     $('#espresso-menu-list').innerHTML += menuItemTemplate(expressoMenuName)
+    coffeMenu.value = ''
+    updateMenuCount()
   }
+
+  function updateMenuCount() {
+    const liCount = $('#espresso-menu-list').querySelectorAll('li').length
+    $('.menu-count').innerText = `총 ${liCount}개`
+  }
+
+  function updateCoffeeMenu(e) {
+    const $menuName = e.target.closest('li').querySelector('.menu-name')
+    //   가장 가까운 list의 값을 가져옵니다.
+    const menuName = $menuName.innerText
+    const updatedMenuName = prompt('메뉴를 수정하세요', menuName)
+
+    //   menuName에 할당해서는 안된다. menuName는 innerText값만 할당하는 것이기 때문에 수정이 되지 않는다.
+    $menuName.innerText = updatedMenuName
+  }
+
+  function removeMenuName(e) {
+    e.target.closest('li').remove()
+    updateMenuCount()
+  }
+
+  //   메뉴를 입력 받는다.
+  const coffeMenu = $('#espresso-menu-name')
+  coffeMenu.addEventListener('keypress', e => {
+    if (e.key === 'Enter' && coffeMenu.value !== '') {
+      insertCoffeeMenu()
+    }
+  })
+  const menuButton = $('#espresso-menu-submit-button')
+  menuButton.addEventListener('click', () => {
+    coffeMenu.value !== '' && insertCoffeeMenu()
+  })
+
+  //   이벤트 위임을 통한 동적인 자식 태크에 이벤트 전달
+  $('#espresso-menu-list').addEventListener('click', e => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      updateCoffeeMenu(e)
+    } else if (e.target.classList.contains('menu-remove-button')) {
+      if (confirm('삭제하시겠습니까?')) {
+        removeMenuName(e)
+      }
+    }
+  })
 }
 App()
